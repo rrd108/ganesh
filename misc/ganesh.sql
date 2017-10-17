3019834a0e1799324c3ca07fc17e83810c7ecb4e
rrd108 <rrd@1108.cc>
2017-10-17 10:56:22 +0200

    sql update for cakedc/users

--- misc/ganesh.sql
+++ misc/ganesh.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: localhost:3306
--- Létrehozás ideje: 2017. Okt 17. 10:25
+-- Létrehozás ideje: 2017. Okt 17. 10:55
 -- Kiszolgáló verziója: 5.7.19-0ubuntu0.17.04.1
 -- PHP verzió: 7.0.22-0ubuntu0.17.04.1
 
@@ -43,12 +43,35 @@ CREATE TABLE `activities` (
 --
 
 CREATE TABLE `activities_users` (
-  `user_id` smallint(5) UNSIGNED NOT NULL,
-  `activitie_id` mediumint(8) UNSIGNED NOT NULL,
+  `id` mediumint(8) UNSIGNED NOT NULL,
+  `user_id` char(36) NOT NULL,
+  `activity_id` mediumint(8) UNSIGNED NOT NULL,
   `start` datetime DEFAULT NULL,
   `end` datetime DEFAULT NULL,
   `fulltime` tinyint(1) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `cake_d_c_users_phinxlog`
+--
+
+CREATE TABLE `cake_d_c_users_phinxlog` (
+  `version` bigint(20) NOT NULL,
+  `migration_name` varchar(100) DEFAULT NULL,
+  `start_time` timestamp NULL DEFAULT NULL,
+  `end_time` timestamp NULL DEFAULT NULL,
+  `breakpoint` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- A tábla adatainak kiíratása `cake_d_c_users_phinxlog`
+--
+
+INSERT INTO `cake_d_c_users_phinxlog` (`version`, `migration_name`, `start_time`, `end_time`, `breakpoint`) VALUES
+(20150513201111, 'Initial', '2017-10-17 06:50:10', '2017-10-17 06:50:10', 0),
+(20161031101316, 'AddSecretToUsers', '2017-10-17 06:50:10', '2017-10-17 06:50:10', 0);
 
 -- --------------------------------------------------------
 
@@ -59,7 +82,7 @@ CREATE TABLE `activities_users` (
 CREATE TABLE `departments` (
   `id` tinyint(3) UNSIGNED NOT NULL,
   `place_id` smallint(5) UNSIGNED NOT NULL,
-  `users_id` smallint(5) UNSIGNED NOT NULL,
+  `user_id` char(36) NOT NULL,
   `name` varchar(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -94,15 +117,52 @@ CREATE TABLE `places` (
 -- --------------------------------------------------------
 
 --
+-- Tábla szerkezet ehhez a táblához `social_accounts`
+--
+
+CREATE TABLE `social_accounts` (
+  `id` char(36) NOT NULL,
+  `user_id` char(36) NOT NULL,
+  `provider` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `reference` varchar(255) NOT NULL,
+  `avatar` varchar(255) DEFAULT NULL,
+  `description` text,
+  `link` varchar(255) NOT NULL,
+  `token` varchar(500) NOT NULL,
+  `token_secret` varchar(500) DEFAULT NULL,
+  `token_expires` datetime DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '1',
+  `data` text NOT NULL,
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Tábla szerkezet ehhez a táblához `users`
 --
 
 CREATE TABLE `users` (
-  `id` smallint(5) UNSIGNED NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `phone` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `female` tinyint(1) DEFAULT '1'
+  `id` char(36) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `last_name` varchar(50) DEFAULT NULL,
+  `token` varchar(255) DEFAULT NULL,
+  `token_expires` datetime DEFAULT NULL,
+  `api_token` varchar(255) DEFAULT NULL,
+  `activation_date` datetime DEFAULT NULL,
+  `secret` varchar(32) DEFAULT NULL,
+  `secret_verified` tinyint(1) DEFAULT NULL,
+  `tos_date` datetime DEFAULT NULL,
+  `active` tinyint(1) NOT NULL DEFAULT '0',
+  `is_superuser` tinyint(1) NOT NULL DEFAULT '0',
+  `role` varchar(255) DEFAULT 'user',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -121,9 +181,15 @@ ALTER TABLE `activities`
 -- A tábla indexei `activities_users`
 --
 ALTER TABLE `activities_users`
-  ADD PRIMARY KEY (`user_id`,`activitie_id`),
-  ADD KEY `fk_people_has_activities_activities1_idx` (`activitie_id`),
-  ADD KEY `fk_people_has_activities_people1_idx` (`user_id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_people_has_activities_activities1_idx` (`activity_id`),
+  ADD KEY `fk_activities_users_users1_idx` (`user_id`);
+
+--
+-- A tábla indexei `cake_d_c_users_phinxlog`
+--
+ALTER TABLE `cake_d_c_users_phinxlog`
+  ADD PRIMARY KEY (`version`);
 
 --
 -- A tábla indexei `departments`
@@ -131,7 +197,7 @@ ALTER TABLE `activities_users`
 ALTER TABLE `departments`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_departments_places1_idx` (`place_id`),
-  ADD KEY `fk_departments_users1_idx` (`users_id`);
+  ADD KEY `fk_departments_users1_idx` (`user_id`);
 
 --
 -- A tábla indexei `festivals`
@@ -148,11 +214,17 @@ ALTER TABLE `places`
   ADD UNIQUE KEY `name_UNIQUE` (`name`);
 
 --
+-- A tábla indexei `social_accounts`
+--
+ALTER TABLE `social_accounts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- A tábla indexei `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `name_phone_email` (`name`,`phone`,`email`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- A kiírt táblák AUTO_INCREMENT értéke
@@ -162,6 +234,11 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT a táblához `activities`
 --
 ALTER TABLE `activities`
+  MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT a táblához `activities_users`
+--
+ALTER TABLE `activities_users`
   MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT a táblához `departments`
@@ -179,11 +256,6 @@ ALTER TABLE `festivals`
 ALTER TABLE `places`
   MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT a táblához `users`
---
-ALTER TABLE `users`
-  MODIFY `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT;
---
 -- Megkötések a kiírt táblákhoz
 --
 
@@ -198,21 +270,27 @@ ALTER TABLE `activities`
 -- Megkötések a táblához `activities_users`
 --
 ALTER TABLE `activities_users`
-  ADD CONSTRAINT `fk_people_has_activities_activities1` FOREIGN KEY (`activitie_id`) REFERENCES `activities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_people_has_activities_people1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_activities_users_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_people_has_activities_activities1` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Megkötések a táblához `departments`
 --
 ALTER TABLE `departments`
   ADD CONSTRAINT `fk_departments_places1` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_departments_users1` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_departments_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Megkötések a táblához `festivals`
 --
 ALTER TABLE `festivals`
   ADD CONSTRAINT `fk_festivals_places` FOREIGN KEY (`place_id`) REFERENCES `places` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Megkötések a táblához `social_accounts`
+--
+ALTER TABLE `social_accounts`
+  ADD CONSTRAINT `social_accounts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
